3eecfc88f8ac2212ad7770ad47ad1436584b60d9
wangchuang <wangchuang.space@gmail.com>
2023-08-11 20:38:15 +0800

    [init]: 初始化

---
+++ app/layout.tsx
@@ -0,0 +1,12 @@
+const RootLayout = (
+    { children }: { children: React.ReactNode }
+) => {
+    return (
+        <html>
+            <body>
+                {children}
+            </body>
+        </html>
+    )
+}
+export default RootLayout
