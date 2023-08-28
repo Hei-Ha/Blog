03ee02ff642f6130864204b6f31d977b773b73ce
wangchuang <wangchuang.space@gmail.com>
2023-08-28 19:35:09 +0800

    完善部分样式

--- src/components/MetadataCard.tsx
+++ src/components/MetadataCard.tsx
@@ -13,26 +13,28 @@ export default (props: PropsType) => {
     const {metadata, content} = props
     return <div className='w-full'>
         <Card
+            data-hover={false}
             fullWidth={true}
             isHoverable={true}
             isPressable={true}
             allowTextSelectionOnPress={true}
             isBlurred={true}
+            isFooterBlurred={true}
             radius={'sm'}
-            className='my-2'
+            shadow={'sm'}
+            className='my-2 bg-#fbfbfb'
         >
             <CardHeader>
-                <div className='text-#333333'>
+                <h1 className='text-#333333 text-lg font-medium'>
                     {metadata.title}
-                </div>
+                </h1>
             </CardHeader>
-            <Divider />
-            <CardBody className='flex bg-#fbfbfb'>
+            <CardBody className='flex bg-#fbfbfb text-#a0a0a0'>
                 <span>{metadata.summary}</span>
             </CardBody>
-            <CardFooter className='flex flex-row justify-between'>
+            <CardFooter className='flex flex-row justify-between text-xs pb-6'>
                 <span className='text-#333333'>阅读全文</span>
-                <span className='text-xs text-#a0a0a0'>写于：{metadata.date}</span>
+                <span className='text-#a0a0a0'>写于：{metadata.date}</span>
             </CardFooter>
         </Card>
     </div>
