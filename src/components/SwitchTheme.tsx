14515cf8a5448ae8de25c1805377e6864adfe976
wangchuang <wangchuang.space@gmail.com>
2023-09-01 11:36:21 +0800

    调整theme switch

--- src/components/SwitchTheme.tsx
+++ src/components/SwitchTheme.tsx
@@ -4,14 +4,48 @@ import { useTheme } from 'next-themes';
 import Image from 'next/image';
 import DarkImg from '@src/assets/dark.svg';
 import LightImg from '@src/assets/light.svg';
+import { Switch } from '@nextui-org/switch';
+import {log} from "next/dist/server/typescript/utils";
 
 export default () => {
     const { theme, setTheme } = useTheme();
-    return <Image
-        src={theme === 'dark' ? LightImg : DarkImg}
-        alt="Picture of the author"
-        width={20}
-        height={20}
-        onClick={() => theme == "dark" ? setTheme('light'): setTheme('dark')}
-    />
+    // <Image
+    //     src={theme === 'dark' ? LightImg : DarkImg}
+    //     alt="Picture of the author"
+    //     width={20}
+    //     height={20}
+    //     onClick={() => theme == "dark" ? setTheme('light'): setTheme('dark')}
+    // />
+    const LightIcon = () => {
+        return <Image
+            src={LightImg}
+            alt="Picture of the author"
+            width={20}
+            height={20}
+            onClick={() => theme == "dark" ? setTheme('light'): setTheme('dark')}
+        />
+    }
+    
+    const DarkIcon = () => {
+        return <Image
+            src={DarkImg}
+            alt="Picture of the author"
+            width={20}
+            height={20}
+            onClick={() => theme == "dark" ? setTheme('light'): setTheme('dark')}
+        />
+    }
+    return <div>
+        <Switch
+            defaultSelected={theme === 'light'}
+            size="md"
+            thumbIcon={({ isSelected, className }) =>
+                isSelected ? <LightIcon /> : <DarkIcon />
+            }
+            color='secondary'
+            onValueChange={(isSelected) => {
+                isSelected ? setTheme('light'): setTheme('dark')
+            }}
+        />
+    </div>
 }
