47628055a57554cd6df0df84b7a5769b15a23ab8
wangchuang <wangchuang.space@gmail.com>
2024-02-13 13:25:54 +0800

    暂时兼容背景

--- src/app/blog/[blogTopic]/layout.tsx
+++ src/app/blog/[blogTopic]/layout.tsx
@@ -9,13 +9,13 @@ export default async ( { params, children } ) => {
     const menuData = await getMenuData( params.blogTopic );
     
     return <>
-        <div className="flex">
+        <div className="flex flex-1">
             <div className="sm:hidden w-280px sideMenuScroll overflow-auto sticky top-16 z-30 h-[calc(100vh-64px)] border-r border-solid border-#e7e7e7">
                 <SideMenu listData={menuData} />
             </div>
             <section className={'flex flex-col flex-1 items-center'}>
                 <TopicList listData={menuData} className={'w-full py-3 text-sm md:hidden lg:hidden'} />
-                <div className="sm:w-[100vw]">
+                <div className="sm:w-[100vw] sm:min-h-[calc(100vh-110px)]">
                     {children}
                 </div>
             </section>
