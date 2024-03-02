d860b021182dbd582c96280a58027d6c49609e05
wangchuang <wangchuang.space@gmail.com>
2024-03-02 15:52:39 +0800

    add huggingface testSpace

--- src/app/module/page.tsx
+++ src/app/module/page.tsx
@@ -1,3 +1,14 @@
+'use client'
+
 export default () => {
-    return <div>content me</div>
+    
+    return <div>
+        test hugging face modal
+        <gradio-app
+            src="https://hei-ha-testspace.hf.space"
+            initial_hieght={'500px'}
+            container={false}
+            theme_mode={'light'}
+        />
+    </div>
 }
