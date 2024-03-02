dbac3d6869f5a4899befabfeefdd88fb453aed17
wangchuang <wangchuang.space@gmail.com>
2024-03-02 22:47:09 +0800

    接入完成

--- src/app/module/page.tsx
+++ src/app/module/page.tsx
@@ -1,14 +1,35 @@
 'use client'
+import React, { useState, useEffect } from 'react'
+import { useTheme } from "next-themes";
+import { Skeleton } from "@nextui-org/react";
+
 
 export default () => {
+    const [mounted, setMounted] = useState<boolean>( false )
+    const { theme } = useTheme()
     
-    return <div>
-        test hugging face modal
+    // const handleLoadComplete = () => {
+    //     console.log( "11111" );
+    //     setMounted( true )
+    // }
+    //
+    // useEffect( () => {
+    //     if ( document.getElementsByClassName( 'gradioApp' )[0] ) {
+    //         document.getElementsByClassName( 'gradioApp' )[0].addEventListener( 'render', handleLoadComplete )
+    //     }
+    //     return () => {
+    //         document.getElementsByClassName( 'gradioApp' )[0].removeEventListener( 'render', handleLoadComplete )
+    //     }
+    // }, [] )
+    
+    
+    return <>
         <gradio-app
+            class={ 'gradioApp' }
             src="https://hei-ha-testspace.hf.space"
-            initial_hieght={'500px'}
-            container={false}
-            theme_mode={'light'}
+            initial_hieght={ '500px' }
+            container={ false }
+            theme_mode={ theme === 'dark' ? 'dark' : 'light' }
         />
-    </div>
+    </>
 }
