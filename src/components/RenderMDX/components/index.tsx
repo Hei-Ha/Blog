a7aec3d14098b628b030b6099614f78db1ccd110
wangchuang <wangchuang.space@gmail.com>
2024-04-21 14:45:27 +0800

    删除多余组件

--- src/components/RenderMDX/components/index.tsx
+++ src/components/RenderMDX/components/index.tsx
@@ -1,22 +1,6 @@
-import { Chip, Button } from '@nextui-org/react'
-import ChipLink from './ChipLink'
 import TitleMsg from './TitleMsg'
-
-const ChipDifficult = (
-    { content, className }: { content: string; className?: string; }
-) => {
-    return <Chip
-        size='md'
-        className={`bg-chipBg text-chipTextEasy select-none cursor-pointer text-xs ${className}`}>
-        {content}
-    </Chip>
-}
 
 
 export {
-    Chip,
-    Button,
-    ChipLink,
-    ChipDifficult,
     TitleMsg,
 }
