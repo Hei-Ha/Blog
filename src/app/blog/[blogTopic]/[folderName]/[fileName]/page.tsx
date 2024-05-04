858b11ac7cb623531d0f86ce5924e339649eae8b
wangchuang <wangchuang.space@gmail.com>
2024-05-04 15:44:49 +0800

    [fix]: 修复loading 位置问题

--- src/app/blog/[blogTopic]/[folderName]/[fileName]/page.tsx
+++ src/app/blog/[blogTopic]/[folderName]/[fileName]/page.tsx
@@ -1,15 +1,15 @@
 import React from "react";
 import RenderMDX from "@src/components/RenderMDX";
-import Fetch from '@src/utils/services'
+import Fetch from "@src/utils/services";
 
+export default async ({ params }) => {
+    const fileContent = await Fetch.GET(
+        `api/getFileContent?path=${params.blogTopic}/${params.folderName}/${params.fileName}`
+    );
 
-export default async ({params}) => {
-    const fileContent = await Fetch.GET(`api/getFileContent?path=${params.blogTopic}/${params.folderName}/${params.fileName}`)
-    
     return (
-        <article
-            className="w-full prose dark:prose-invert my-10 prose-base sm:prose-sm sm:px-2 -z-40">
-            <RenderMDX sourceData={fileContent.content || ''}/>
+        <article className="w-full prose dark:prose-invert my-10 prose-base sm:prose-sm sm:px-2 -z-40">
+            <RenderMDX sourceData={fileContent.content || ""} />
         </article>
     );
 };
