65ae15e77b29b2ace549fff1e26334726f582076
wangchuang <wangchuang.space@gmail.com>
2024-05-05 15:39:17 +0800

    [fix]: 修复loading 位置

--- src/components/RenderMDX/components/index.tsx
+++ src/components/RenderMDX/components/index.tsx
@@ -1,8 +1,11 @@
 import TitleMsg from './TitleMsg'
 import DifficultyLevel from './DifficultyLevel'
+import { Chip, Link } from "@nextui-org/react";
 
 
 export {
     TitleMsg,
     DifficultyLevel,
+    Chip,
+    Link,
 }
